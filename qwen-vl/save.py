bcc978e179116c9b54d7a54a1bbdf18aa9c06a88
yuwenhan07 <yuwenhan@buaa.edu.cn>
2025-08-20 15:30:47 +0800

    add new generate data

--- qwen-vl/save.py
+++ qwen-vl/save.py
@@ -2,27 +2,21 @@ import os
 import traceback
 from automatikz.infer import TikzDocument 
 
-# ? groundtruth-tex 目录包含 TikZ 源码文件
-# input_dir = "./output/groundtruth-tex"  # 输入目录，包含 .tex 文件
-# output_dir = "./tikz_output/groundtruth-pdf&png"  # 输出目录，保存 PDF 和 PNG 文件
-# os.makedirs(output_dir, exist_ok=True)
-
 # ? 如果需要处理 output/output-tex 中的文件，可以取消注释以下两行
-input_dir = "./output/output-img"  # 输入目录，包含 .tex 文件
-output_dir = "./output/output-img-pdf&png"  # 输出目录，保存 PDF 和 PNG 文件
-os.makedirs(output_dir, exist_ok=True)
-
-start_processing = False  # Variable to track when to start processing
+# input_dir = "./output/output-tex"  # 输入目录，包含 .tex 文件
+# output_pdf_dir = "./compile_output/pdf"  
+# output_png_dir = "./compile_output/png"  
+# output_log_dir = "./compile_output/log"
+input_dir = "./output/output-tex-inputwithimg"  # 输入目录，包含 .tex 文件
+output_pdf_dir = "./compile_output/pdf-inputwithimg"  
+output_png_dir = "./compile_output/png-inputwithimg"  
+output_log_dir = "./compile_output/log-inputwithimg"
+os.makedirs(output_pdf_dir, exist_ok=True)
+os.makedirs(output_png_dir, exist_ok=True)
+os.makedirs(output_log_dir, exist_ok=True)
 
 for filename in os.listdir(input_dir):
     if filename.endswith(".tex"):
-        # Start processing from the specified file
-        if filename == "sample_11.tex":
-            start_processing = True
-
-        # Skip files before the specified file
-        if not start_processing:
-            continue
 
         tex_path = os.path.join(input_dir, filename)
 
@@ -45,20 +39,20 @@ for filename in os.listdir(input_dir):
         try:
             # 保存 PDF
             if tikzdoc.pdf:
-                pdf_path = os.path.join(output_dir, name_without_ext + ".pdf")
+                pdf_path = os.path.join(output_pdf_dir, name_without_ext + ".pdf")
                 tikzdoc.save(pdf_path)
                 print(f"✅ 已保存 PDF 到 {pdf_path}")
 
             # 保存 PNG
             if tikzdoc.has_content:
-                png_path = os.path.join(output_dir, name_without_ext + ".png")
+                png_path = os.path.join(output_png_dir, name_without_ext + ".png")
                 img = tikzdoc.rasterize()
                 img.save(png_path)
                 print(f"✅ 已保存 PNG 到 {png_path}")
 
             # 编译失败时输出日志
             if tikzdoc.compiled_with_errors:
-                log_path = os.path.join(output_dir, name_without_ext + ".log")
+                log_path = os.path.join(output_log_dir, name_without_ext + ".log")
                 with open(log_path, "w", encoding="utf-8") as log_file:
                     log_file.write(tikzdoc.log)
                 print(f"⚠️ 编译 {filename} 时出错！日志已保存至 {log_path}")
